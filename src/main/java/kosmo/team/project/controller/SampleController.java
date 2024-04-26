a15e532edabe7ddaec87102d8bfd4ee945e27ba1
dabin0724 <dabin072482@gmail.com>
2024-04-26 12:20:29 +0900

    테스트

--- src/main/java/kosmo/team/project/controller/SampleController.java
+++ src/main/java/kosmo/team/project/controller/SampleController.java
@@ -3,6 +3,7 @@ package kosmo.team.project.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+///   깃 테스트
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
